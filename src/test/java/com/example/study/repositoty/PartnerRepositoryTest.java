d97e0ce276faf9af4e466b488dec76834f90ae6e
zidol <ajh3166@naver.com>
2020-05-03 21:06:31 +0900

    partmer Repository test

--- src/test/java/com/example/study/repositoty/PartnerRepositoryTest.java
+++ src/test/java/com/example/study/repositoty/PartnerRepositoryTest.java
@@ -39,7 +39,7 @@ public class PartnerRepositoryTest extends DemoApplicationTests {
         partner.setRegisteredAt(registeredAt);
         partner.setCreatedAt(createdAt);
         partner.setCreatedBy(createdBy);
-        partner.setCategoryId(categoryId);
+//        partner.setCategoryId(categoryId);
 
         Partner newPartner = partnerRepository.save(partner);
 
