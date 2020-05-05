9a06f1d7db4207d2f1fa6daede41ad7cb60eb76b
zidol <ajh3166@naver.com>
2020-05-05 16:10:59 +0900

    추상화 컨트롤러 (코드 리팩토링 작업)

--- src/main/java/com/example/study/controller/api/ItemApiController.java
+++ src/main/java/com/example/study/controller/api/ItemApiController.java
@@ -8,34 +8,17 @@ import com.example.study.service.ItemApiLogicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
+
 @RestController
 @RequestMapping("/api/item")
-public class ItemApiController implements CrudInterface<ItemApiRequest, ItemApiResponse> {
+public class ItemApiController extends CrudController<ItemApiRequest, ItemApiResponse> {
 
     @Autowired
     private ItemApiLogicService itemApiLogicService;
 
-    @Override
-    @PostMapping("")
-    public Header<ItemApiResponse> create(@RequestBody Header<ItemApiRequest> request) {
-        return itemApiLogicService.create(request);
-    }
-
-    @Override
-    @GetMapping("{id}")
-    public Header<ItemApiResponse> read(@PathVariable Long id) {
-        return itemApiLogicService.read(id);
-    }
-
-    @Override
-    @PutMapping("")
-    public Header<ItemApiResponse> update(@RequestBody Header<ItemApiRequest> request) {
-        return itemApiLogicService.update(request);
-    }
-
-    @Override
-    @DeleteMapping("{id}")
-    public Header delete(@PathVariable Long id) {
-        return itemApiLogicService.delete(id);
+    @PostConstruct
+    public void init() {
+        this.baseService = itemApiLogicService;
     }
 }
--- src/main/java/com/example/study/controller/api/OrderGroupApiController.java
+++ src/main/java/com/example/study/controller/api/OrderGroupApiController.java
@@ -8,36 +8,17 @@ import com.example.study.service.OrderGroupApiLogicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
+
 @RestController
 @RequestMapping("api/orderGroup")
-public class OrderGroupApiController implements CrudInterface<OrderGroupApiRequest, OrderGroupApiResponse> {
+public class OrderGroupApiController extends CrudController<OrderGroupApiRequest, OrderGroupApiResponse> {
 
     @Autowired
     private OrderGroupApiLogicService orderGroupApiLogicService;
 
-    @Override
-    @PostMapping("")
-    public Header<OrderGroupApiResponse> create(@RequestBody Header<OrderGroupApiRequest> request) {
-
-        return orderGroupApiLogicService.create(request);
-    }
-
-    @Override
-    @GetMapping("{id}")
-    public Header<OrderGroupApiResponse> read(@PathVariable Long id) {
-        return orderGroupApiLogicService.read(id);
-    }
-
-    @Override
-    @PutMapping("")
-    public Header<OrderGroupApiResponse> update(@RequestBody Header<OrderGroupApiRequest> request) {
-
-        return orderGroupApiLogicService.update(request);
-    }
-
-    @Override
-    @DeleteMapping("{id}")
-    public Header delete(@PathVariable Long id) {
-        return orderGroupApiLogicService.delete(id);
-    }
+   @PostConstruct
+    public void init() {
+       this.baseService = orderGroupApiLogicService;
+   }
 }
